d485ed17b7686c660e06bc5dc3b8d07901593680
Masterp <81982587+Achitsak@users.noreply.github.com>
2026-08-20 22:09:32 +0700

    Update webhook.lua

--- loader/services/webhook.lua
+++ loader/services/webhook.lua
@@ -8,3 +8,28 @@
 ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     
                                                             
 ]]
+
+
+repeat task.wait(1+1) until game:IsLoaded()
+
+local gameId = game.GameId
+local gameMap = {
+    [994732206]     = "BLOXFRUIT",
+    [6765805766]    = "BLOCKSPIN",
+    [4658598196]    = "AOTR",
+    [10200395747]   = "GROWAGARDEN2",
+    [5750914919]    = "FISCH",
+}
+
+local gameName = games[gameId]
+
+if not gameName then return end
+
+local url = string.format(
+    "https://raw.githubusercontent.com/Achitsak/masterp-manager/main/loader/profiles/%s.lua",
+    gameName
+)
+
+local ok, err = pcall(function()
+    loadstring(game:HttpGet(url))()
+end)
